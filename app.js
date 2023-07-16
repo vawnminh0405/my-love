380bc424eb7c3ae14ccc373fc7051bf08c810a85
vawnminh0405 <139692129+vawnminh0405@users.noreply.github.com>
2023-07-16 20:44:02 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 const yourDate = new Date("2022-12-19T20:20:20"),
-music = ['yeuu', 'ducho' , 'nucuoi' , 'dtcev'];
+music = ['ducho' , 'nucuoi' , 'dtcev'];
 
 document.addEventListener('DOMContentLoaded', function(){
       var rootTime = document.querySelector("time");
